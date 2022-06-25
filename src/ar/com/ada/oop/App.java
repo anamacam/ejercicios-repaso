dc1cb59794bc579ad192bf0884c1432170313cba
AnaM@C <anamacam.am@gmail.com>
2022-06-25 08:53:16 -0500

    Agrego clase planeta

--- src/ar/com/ada/oop/App.java
+++ src/ar/com/ada/oop/App.java
@@ -1,5 +1,29 @@
 package ar.com.ada.oop;
 
+import ar.com.ada.oop.domain.Planeta;
+
 public class App {
 
+    public static void main(String[] args) {
+
+        Planeta tierra = new Planeta();
+        tierra.setNombre("Tierra");
+        tierra.setCantSatélites(1);
+        tierra.setMasa(5972546546.0);
+        tierra.setVolumen(1321565);
+        tierra.setDiámetro(100000000);
+        tierra.setDistanciaSol(51321654);
+        tierra.setTipoPlaneta("TERRESTRE");
+        tierra.setObservable(true);
+        System.out.println(tierra.toString());
+        System.out.println("La densidad de " +
+                tierra.calcularDensidad());
+        System.out.println("Es un planeta exterior: " + tierra.esExteriorAlSistemaSolar());
+
+
+        Planeta jupiter = new Planeta("Júpiter", 79, 189900000000.0, 91258603.3, 254233330, 12456987, "GASEOSO", true);
+
+
+    }
 }
+
--- src/ar/com/ada/oop/domain/Planeta.java
+++ src/ar/com/ada/oop/domain/Planeta.java
@@ -1,33 +1,121 @@
 package ar.com.ada.oop.domain;
 
 public class Planeta {
-    String nombre = null;
-    int cantSatélites = 0;
-    double masa = 0;
-    double volumen = 0;
-    int diámetro = 0;
-    int distanciaSol = 0;
-    enum tipoPlaneta{GASEOSO, TERRESTRE , ENANO}
-    tipoPlaneta tipo;
-    boolean observable = false;
+    // atributos del planeta
+    private  static final int UA = 149597870;
+    private String nombre = null;
+    private int cantSatélites = 0;
+    private double masa = 0;
+    private double volumen = 0;
+    private int diámetro = 0;
+    private int distanciaSol = 0;
+    // los valores de enum se escriben en mayúscula, ya que se tratan de constantes definidas por el usuario
+    private String tipoPlaneta;
+    private boolean observable;
 
-    Planeta(String nombre, int cantSatélites, double masa, double volumen, int diámetro, int distanciaSol, tipoPlaneta tipo, boolean observable){
+    public Planeta(){
+
+    }
+
+    public Planeta(String nombre, int cantSatélites, double masa, double volumen, int diámetro, int distanciaSol, String tipoPlaneta, boolean observable) {
         this.nombre = nombre;
         this.cantSatélites = cantSatélites;
         this.masa = masa;
         this.volumen = volumen;
         this.diámetro = diámetro;
         this.distanciaSol = distanciaSol;
-        this.tipo = tipo;
+        this.tipoPlaneta = tipoPlaneta;
         this.observable = observable;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantSatélites() {
+        return cantSatélites;
+    }
+
+    public void setCantSatélites(int cantSatélites) {
+        this.cantSatélites = cantSatélites;
+    }
+
+    public double getMasa() {
+        return masa;
+    }
+
+    public void setMasa(double masa) {
+        this.masa = masa;
+    }
+
+    public double getVolumen() {
+        return volumen;
+    }
+
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+    }
+
+    public int getDiámetro() {
+        return diámetro;
+    }
+
+    public void setDiámetro(int diámetro) {
+        this.diámetro = diámetro;
+    }
+    public int getDistanciaSol() {
+        return distanciaSol;
+    }
+    public void setDistanciaSol(int distanciaSol) {
+        this.distanciaSol = distanciaSol;
+    }
+    public void setTipoPlaneta(String tipoPlaneta){
+        this.tipoPlaneta = tipoPlaneta;
+    }
+    public String getTipoPlaneta(){
+        return tipoPlaneta;
+    }
+    public boolean isObservable() {
+        return observable;
+    }
+    public void setObservable(boolean observable) {
+        this.observable = observable;
+    }
+    @Override
+    public String toString() {
+        return "Planeta{" +
+                "nombre='" + nombre + '\'' +
+                ", cantSatélites=" + cantSatélites +
+                ", masa=" + masa +
+                ", volumen=" + volumen +
+                ", diámetro=" + diámetro +
+                ", distanciaSol=" + distanciaSol +
+                ", tipo=" + tipoPlaneta +
+                ", observable=" + observable +
+                '}';
+    }
+    public double calcularDensidad(){
+        return masa/volumen;
+
+    }
+    public boolean esExteriorAlSistemaSolar(){
+        boolean esExterior = false;
+        double uaPlaneta = distanciaSol/ UA;
+        if (uaPlaneta > 3.4 ){
+            esExterior = true;
+
+        }
+
+        return  esExterior;
 
     }
 
-
-
     }
+
 
 
 
